33a2e6a573e8cd07f608938e86f5e5471a4d557f
Victor "multun" Collod <victor.collod@epita.fr>
2020-06-07 03:44:49 -0700

    document the sphinx configuration

--- conf.py
+++ conf.py
@@ -15,39 +15,52 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinxcontrib.bibtex",
     "sphinxcontrib.rsvgconverter",
-    "advanced_admonition",
     "sphinx.ext.imgmath",
+    # advanced_admonition is a custom extension to render
+    # the .. advanced:: warnings. the source is in advanced_admonition.py
+    "advanced_admonition",
 ]
 
+# number figures
 numfig = True
 
-templates_path = ["_templates"]
-
+# only parse rst files
 source_suffix = ".rst"
 
+# paths to exclude from the document source scan
+exclude_patterns = [
+    "_build",
+    "_build_*",
+    "Thumbs.db",
+    ".DS_Store",
+    ".venv",
+]
+
+# the name of the root document
 master_doc = "index"
 
+# these can be read inside the .rst, and are also used in the latex preamble + sphinx stuff
 project = "Crypto 101"
 copyright = "2020, Laurens Van Houtven (lvh)"
 author = "lvh"
 
+# these can be accessed as |version| and |release| inside the .rst source
 import subprocess
-
 version = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode()
 release = subprocess.check_output(["git", "describe"]).decode()
 
-
+# i18n configuration
 locale_dirs = ["locale/"]
 gettext_compact = False
 
-exclude_patterns = ["_build", "_build_*", "Thumbs.db", ".DS_Store", ".venv"]
 
 pygments_style = "sphinx"
 
+# whether to show .. todo:: 's into the built document
 todo_include_todos = True
 
-html_theme = "alabaster"
 # https://alabaster.readthedocs.io/en/latest/customization.html#theme-options
+html_theme = "alabaster"
 html_theme_options = {
     "show_relbars": True,
     "fixed_sidebar": True,
@@ -73,15 +86,16 @@ def read_latex_template(name: str) -> str:
     return Template(read_latex_source(name)).substitute(globals())
 
 
+# inline math is rendered as svg, and needs a preamble to render properly
+imgmath_latex_preamble = read_latex_source("imgmath")
 imgmath_image_format = "svg"
 imgmath_font_size = 16
 
-imgmath_latex_preamble = read_latex_source("imgmath")
-
+# whether to show the page number after references
 latex_show_pagerefs = True
-
 latex_engine = "xelatex"
 
+# what documents to build
 latex_documents = [
     (
         # master document
@@ -97,11 +111,10 @@ latex_documents = [
     )
 ]
 
+# top level titles are parts
 latex_toplevel_sectioning = "part"
 
 latex_docclass = {"manual": "memoir"}
-
-
 latex_elements = {
     "printindex": "",
     "pointsize": "11pt",
@@ -133,6 +146,7 @@ latex_elements = {
 
 latex_additional_files = ["./Illustrations/CC/CC-BY-NC.pdf"]
 
+# the text shown by the ".. advanced::" admonition
 advanced_admonition_text = str(
     _(
         "This is an optional, in-depth section. It almost certainly won't help you write better software, "
